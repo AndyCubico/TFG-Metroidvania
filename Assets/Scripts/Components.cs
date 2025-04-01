ed127b73aee41f96d37a979f56da5d9d0257635a
lSara-MM <sqmartinezm@gmail.com>
2025-04-01 09:55:53 +0200

    remove unnecessary

--- Assets/Scripts/Components.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using UnityEngine;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Transforms;
-
--- Assets/Scripts/Components/SpeedAuthoring.cs
+++ Assets/Scripts/Components/SpeedAuthoring.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class SpeedAuthoring : MonoBehaviour
 {
     public Vector2 speed;
+
     public class Baker : Baker<SpeedAuthoring>
     {
         public override void Bake(SpeedAuthoring authoring)
         {
-            Entity entity = GetEntity(TransformUsageFlags.Dynamic); //Changed from WorldSpace to Dynamic
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
             AddComponent(entity, new SpeedComponent
             {
                 value = authoring.speed

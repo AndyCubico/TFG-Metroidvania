aed13d7dc5e30d25beb48b781422790d4556dc8c
AndyCubico <astartes99@gmail.com>
2025-04-01 17:57:52 +0200

    Job example in UnitMoverSystem

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -3,12 +3,23 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
 using Unity.Physics;
+using System.Diagnostics;
 
 partial struct UnitMoverSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        UnitMoverJob unitMoverJob = new UnitMoverJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime,
+        };
+
+        unitMoverJob.ScheduleParallel();
+                    
+
+        // Both examples without jobs
+
         // No physics
         //foreach ((
         //    RefRW<LocalTransform> localTransform,
@@ -22,18 +33,33 @@ partial struct UnitMoverSystem : ISystem
         //}
 
         // Physics
-        foreach ((
-            RefRW<LocalTransform> localTransform,
-            RefRO<SpeedComponent> moveSpeed,
-            RefRW<PhysicsVelocity> physicsVelocity)
-            in SystemAPI.Query<
-                RefRW<LocalTransform>,
-                RefRO<SpeedComponent>,
-                RefRW<PhysicsVelocity>>())
+        //foreach ((
+        //    RefRW<LocalTransform> localTransform,
+        //    RefRO<SpeedComponent> moveSpeed,
+        //    RefRW<PhysicsVelocity> physicsVelocity)
+        //    in SystemAPI.Query<
+        //        RefRW<LocalTransform>,
+        //        RefRO<SpeedComponent>,
+        //        RefRW<PhysicsVelocity>>())
+        //{
+        //    physicsVelocity.ValueRW.Linear.x = moveSpeed.ValueRO.value.x;
+        //    physicsVelocity.ValueRW.Linear.y = moveSpeed.ValueRO.value.y;
+        //    physicsVelocity.ValueRW.Angular = float3.zero;
+        //}
+    }
+
+    [BurstCompile]
+    public partial struct UnitMoverJob : IJobEntity
+    {
+        public float deltaTime; // Example of how to use external variables
+
+        public void Execute(ref LocalTransform localTransform, in SpeedComponent moveSpeed, ref PhysicsVelocity physicsVelocity)
         {
-            physicsVelocity.ValueRW.Linear.x = moveSpeed.ValueRO.value.x;
-            physicsVelocity.ValueRW.Linear.y = moveSpeed.ValueRO.value.y;
-            physicsVelocity.ValueRW.Angular = float3.zero;
+            physicsVelocity.Linear.x = moveSpeed.value.x;
+            physicsVelocity.Linear.y = moveSpeed.value.y;
+            physicsVelocity.Angular = float3.zero;
+
+            Debug.WriteLine(deltaTime);
         }
     }
 }

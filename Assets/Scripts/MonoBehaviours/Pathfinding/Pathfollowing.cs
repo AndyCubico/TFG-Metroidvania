a08c415d0de3d2f0d7386eec1884f6b110a3bde8
AndyCubico <astartes99@gmail.com>
2025-05-02 19:59:54 +0200

    WIP movement AI agent

--- Assets/Scripts/MonoBehaviours/Pathfinding/Pathfollowing.cs
+++ Assets/Scripts/MonoBehaviours/Pathfinding/Pathfollowing.cs
@@ -7,6 +7,13 @@ public class Pathfollowing : MonoBehaviour
     private NativeList<int2> m_Path;
     private int m_PathIndex = 0; // Index to keep track of the next step to follow.
     [SerializeField] private float m_Speed;
+    [SerializeField] private float m_JumpForce;
+    [SerializeField] private bool m_IsGrounded;
+
+    private Rigidbody2D m_rb;
+    [SerializeField] private LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
 
     // Debug
     [SerializeField] private int2 m_StartPosition;
@@ -15,6 +22,7 @@ public class Pathfollowing : MonoBehaviour
     private void Awake()
     {
         m_Path = new NativeList<int2>(Allocator.Persistent);
+        m_rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -33,9 +41,10 @@ public class Pathfollowing : MonoBehaviour
 
             transform.position += moveDirection * m_Speed * Time.deltaTime;
 
-            if (moveDirection.y > 0)
+            if (CheckJump(transform.position))
             {
                 // Jump
+                Jump();
             }
 
             if (math.distance(transform.position, targetPosition) < 0.01f)
@@ -46,6 +55,28 @@ public class Pathfollowing : MonoBehaviour
         }
     }
 
+    private void MoveToX(Vector3 targetPosition)
+    {
+        float directionX = targetPosition.x - transform.position.x;
+        directionX = Mathf.Sign(directionX); // -1, 0 or 1
+
+        transform.position += new Vector3(directionX, 0f, 0f) * m_Speed * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        m_rb.linearVelocity += new Vector2(0, m_JumpForce);
+    }
+
+    private bool CheckJump(Vector3 targetPosition)
+    {
+        return targetPosition.y > transform.position.y + 0.1f && m_IsGrounded; // Add 0.1f to avoid jumping when the difference in y is too small. 
+    }
+
+    private bool CheckIsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
 
     /// <summary>
     /// Set the path for the agent to follow.

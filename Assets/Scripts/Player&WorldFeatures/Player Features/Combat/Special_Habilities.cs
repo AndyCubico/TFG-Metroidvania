81fe827ecb681bce871421e55b6adb2873f76241
TheimerTR <albertohidalgo268@gmail.com>
2025-05-13 12:24:12 +0200

    Special Attack snow WIP1

--- Assets/Scripts/Player&WorldFeatures/Player Features/Combat/Special_Habilities.cs
+++ Assets/Scripts/Player&WorldFeatures/Player Features/Combat/Special_Habilities.cs
@@ -1,4 +1,5 @@
 using PlayerController;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,11 +14,21 @@ public class Special_Habilities : MonoBehaviour
     public Rigidbody2D rb;
     bool rigidbodyFreeze;
 
+    [Header("Habilities Time")]
+    [Space(5)]
+    public float snowAttackDuration;
+    private float snowAttackTimer;
+
+    float defaultGravity;
+
+    bool isSnowAttacking;
+
     [Header("Input Actions")]
     [Space(5)]
     public InputActionReference SpecialHabilitiesTrigger;
     public InputActionReference SnowHability;
 
+    bool usingController;
     bool specialHabilitiesTrigger;
     bool snowHability;
 
@@ -61,34 +72,101 @@ public class Special_Habilities : MonoBehaviour
     void Start()
     {
         rigidbodyFreeze = false;
+        isSnowAttacking = false;
+        defaultGravity = rb.gravityScale;
+        snowAttackTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!characterPlayerController.isDashing)
+        if (Gamepad.current != null)
         {
-            characterPlayerController.enabled = !specialHabilitiesTrigger;
+            usingController = true;
+        }
+        // Verifica si hay un teclado conectado
+        else if (Keyboard.current != null)
+        {
+            usingController = false;
+        }
+        // Si no hay ni teclado ni mando conectado, muestra un mensaje de advertencia
+        else
+        {
+            usingController = false;
+        }
 
-            if (specialHabilitiesTrigger && !rigidbodyFreeze)
+        if (!characterPlayerController.isDashing)
+        {
+            if (usingController)
             {
-                rb.linearVelocity = Vector3.zero;
-                rigidbodyFreeze = true;
-            }
-            else if (!specialHabilitiesTrigger)
-            {
-                rigidbodyFreeze = false;
-            }
+                //characterPlayerController.enabled = !specialHabilitiesTrigger;
 
-            if (specialHabilitiesTrigger && snowHability)
-            {
-                SnowSpecialAttack();
+                //if (specialHabilitiesTrigger && !rigidbodyFreeze)
+                //{
+                //    rb.linearVelocity = Vector3.zero;
+                //    rigidbodyFreeze = true;
+                //}
+                //else if (!specialHabilitiesTrigger)
+                //{
+                //    rigidbodyFreeze = false;
+                //}
+
+                if (specialHabilitiesTrigger && snowHability && !isSnowAttacking && snowAttackTimer <= 0)
+                {
+                    snowAttackTimer = snowAttackDuration;
+
+                    SnowSpecialAttack();
+                }
             }
+            else
+            {
+                //if (isSnowAttacking /*More abilities*/)
+                //{
+                //    if (!rigidbodyFreeze)
+                //    {
+                //        rb.linearVelocity = Vector3.zero;
+                //        rb.gravityScale = 0;
+                //        rigidbodyFreeze = true;
+                //    }
+                //}
+
+                if (snowHability && !isSnowAttacking && snowAttackTimer <= 0)
+                {
+                    snowAttackTimer = snowAttackDuration;
+
+                    StartCoroutine(SnowSpecialAttack());
+                }
+            }
+        }
+
+        if (snowAttackTimer > 0 && !isSnowAttacking)
+        {
+            snowAttackTimer -= Time.deltaTime;
+        }
+
+        if (!isSnowAttacking /*Other abilities*/)
+        {
+            characterPlayerController.enabled = true;
+            rb.gravityScale = defaultGravity;
+            rigidbodyFreeze = false;
         }
     }
 
-    void SnowSpecialAttack()
+    public IEnumerator SnowSpecialAttack()
     {
+        characterPlayerController.enabled = false;
+        isSnowAttacking = true;
+
+        //while (!characterPlayerController.isGrounded)
+        //{
+
+        //}
+
         Debug.Log("Snow Attack!");
+
+        yield return new WaitForSeconds(2f);
+        
+        isSnowAttacking = false;
+        Debug.Log("Snow Attack Finished!");
     }
 }

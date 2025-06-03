a641b5646c61d916c4cfa4289c007f679ffadf30
TheimerTR <albertohidalgo268@gmail.com>
2025-06-03 14:09:25 +0200

    Heavy Attack WIP

--- Assets/Scripts/Player&WorldFeatures/Player Features/AnimationManager.cs
+++ Assets/Scripts/Player&WorldFeatures/Player Features/AnimationManager.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour
 {
     public PlayerCombat playerCombat;
+    public HeavyAttack heavyAttack;
 
     public void BasicAttackCombatAnimationHasEnded()
     {
         playerCombat.AnimationHasFinished();
+    }
+
+    public void HeavyAttackHasEnded()
+    {
+        heavyAttack.AnimationHasFinished();
+    }
+
+    public void HeavyAttackHit()
+    {
+        heavyAttack.Hit();
     }
 }

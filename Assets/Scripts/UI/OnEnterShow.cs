9fb2c712101972867dad200c61dd6599d50f7299
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2025-06-19 16:54:49 +0200

    upgrade

--- Assets/Scripts/UI/OnEnterShow.cs
+++ Assets/Scripts/UI/OnEnterShow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OnEnterShow : MonoBehaviour
 {
     [SerializeField] GameObject m_ObjectToShow;
+    [SerializeField] bool isHide;
     [TagDropdown] public string[] collisionTag = new string[] { };
 
     public float timeForShow = 0.0f; // Time the gameObject must stay inside to make hidden object visible.
@@ -23,7 +24,7 @@ public class OnEnterShow : MonoBehaviour
             }
             else
             {
-                m_ObjectToShow.SetActive(true);
+                m_ObjectToShow.SetActive(!isHide);
             }
         }
 
@@ -33,7 +34,7 @@ public class OnEnterShow : MonoBehaviour
     {
         if (triggeringObject == collision.gameObject) // Only when the og go moves out, it stops showwing
         {
-            m_ObjectToShow.SetActive(false);
+            m_ObjectToShow.SetActive(isHide);
             triggeringObject = null;
         }
     }

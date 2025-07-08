425843e063df2282c1e0c78bdf564e952fbd6b5c
AndyCubico <astartes99@gmail.com>
2025-07-08 18:38:32 +0200

    Update Pathfollowing.cs

--- Assets/Scripts/MonoBehaviours/Pathfinding/Pathfollowing.cs
+++ Assets/Scripts/MonoBehaviours/Pathfinding/Pathfollowing.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
-using static UnityEngine.EventSystems.EventTrigger;
-using static UnityEngine.UI.Image;
 
 public class Pathfollowing : MonoBehaviour
 {

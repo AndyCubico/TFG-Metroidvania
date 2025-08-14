7bfabd94281a0750ea4e99aa58b33cd974ff343e
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2025-08-14 13:19:56 +0200

    Snow WIP

---
+++ Assets/Scripts/Player&WorldFeatures/World Features/WeatherElemenents/SnowBehaviour.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SnowBehaviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        WeatherManager.ChangeWeather += UpdateSnow;
+    }
+
+    private void OnDisable()
+    {
+        WeatherManager.ChangeWeather -= UpdateSnow;
+    }
+
+    public void UpdateSnow(CLIMATES c)
+    {
+        if (WeatherManager.Instance.GetExteriorState()) // If is an interior area weather doesn't affect.
+        {
+            switch (c)
+            {
+                case CLIMATES.NEUTRAL:
+
+                    gameObject.GetComponent<Renderer>().enabled = true;
+
+                    break;
+                case CLIMATES.SUN:
+
+                    gameObject.GetComponent<Renderer>().enabled = false;
+
+                    break;
+                case CLIMATES.SNOW:
+
+                    gameObject.GetComponent<Renderer>().enabled = true;
+
+                    break;
+                case CLIMATES.NONE:
+                    break;
+            }
+        }
+    }
+}

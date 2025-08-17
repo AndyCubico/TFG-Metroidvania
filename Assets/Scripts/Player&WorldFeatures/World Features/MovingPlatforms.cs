5b2906eb4dcf8240cf87654224406533561ed6e4
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2025-08-17 19:46:56 +0200

    prefab not slipery

--- Assets/Scripts/Player&WorldFeatures/World Features/MovingPlatforms.cs
+++ Assets/Scripts/Player&WorldFeatures/World Features/MovingPlatforms.cs
@@ -1,25 +1,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MovingPlatforms : MonoBehaviour
 {
     [SerializeField] Dictionary<GameObject, Transform> m_ObjectOriginalParent;
+    [SerializeField] List<Transform> m_Objects;
     [TagDropdown] public string[] collisionTag = new string[] { };
+    Vector3 m_LastPosition;
+    Vector3 m_Displacement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_ObjectOriginalParent = new Dictionary<GameObject, Transform> { };
+        m_LastPosition = transform.position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collisionTag.Contains(collision.gameObject.tag) && !m_ObjectOriginalParent.ContainsKey(collision.gameObject)) 
+        if (collisionTag.Contains(collision.gameObject.tag) && !m_ObjectOriginalParent.ContainsKey(collision.gameObject))
         {
-            m_ObjectOriginalParent.Add(collision.gameObject,collision.transform.parent);
+            m_ObjectOriginalParent.Add(collision.gameObject, collision.transform.parent);
             collision.gameObject.transform.parent = this.transform;
         }
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collisionTag.Contains(collision.gameObject.tag) && !m_Objects.Contains(collision.transform))
+    //    {
+    //        m_Objects.Add(collision.transform);
+    //    }
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    m_Displacement = m_LastPosition - transform.position;
+
+    //    for (int i = 0; i < m_Objects.Count; i++)
+    //    {
+    //        m_Objects[i].position += m_Displacement;
+    //    }
+
+    //    m_LastPosition = transform.position;
+    //}
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -31,4 +56,12 @@ public class MovingPlatforms : MonoBehaviour
             m_ObjectOriginalParent.Remove(collision.gameObject);
         }
     }
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (m_Objects.Contains(collision.transform))
+    //    {
+    //        m_Objects.Remove(collision.transform);
+    //    }
+    //}
 }

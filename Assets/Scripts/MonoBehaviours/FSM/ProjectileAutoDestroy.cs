eefd31be8fa5a61e71e47f17889aa1121430e497
AndyCubico <astartes99@gmail.com>
2025-08-31 17:58:00 +0200

    Proejectile autodestroy

--- Assets/Scripts/MonoBehaviours/FSM/ProjectileAutoDestroy.cs
+++ Assets/Scripts/MonoBehaviours/FSM/ProjectileAutoDestroy.cs
@@ -3,36 +3,17 @@ using UnityEngine;
 public class ProjectileAutoDestroy : MonoBehaviour
 {
     public float lifetime = 2f;
+    public LayerMask m_GroundLayer;
 
     void Start()
     {
         Destroy(gameObject, lifetime);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.gameObject.activeSelf)
-        //{
-        //    // Check recursively up the parent chain for "Enemy" tag
-        //    Transform current = collision.transform;
-        //    bool isEnemy = false;
-        //    while (current != null)
-        //    {
-        //        if (current.CompareTag("Enemy"))
-        //        {
-        //            isEnemy = true;
-        //            break;
-        //        }
-        //        current = current.parent;
-        //    }
-
-        //    if (!isEnemy)
-        //    {
-        //        Destroy(gameObject);
-        //    }
-        //}
-
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("NormalFloor"))
+        if (((1 << collision.gameObject.layer) & m_GroundLayer) != 0)
         {
             Destroy(gameObject);
         }
